1a13be8b063b4e8bb13fa7f85d177a075559a9eb
Glitch (blushing-ionized-neighbor) <none>
2021-12-09 11:14:26 +0000

    🌮🛩 Checkpoint ./package.json:19048167/5042 ./watch.json:19048167/8 ./commands/join.js:19048167/7347

---
+++ commands/join.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "join",
+  alias:["j"],
+  description: "Join voice channel",
+  run: async(client, message) => {
+if(!message.member.voice.channel) return message.channel.send("Please connect to a voice channel!"); //If you are not in the voice channel, then return a message
+message.member.voice.channel.join(); //Join the voice channel
+    
+  }
+}
